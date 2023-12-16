089b3712ff0475e53c3b2fbfa8b5d3c96e6170c0
ouma-kaltoum <101124869+ouma-kaltoum@users.noreply.github.com>
2023-12-16 20:56:02 +0100

    vider le formulaire d'ajout

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const statutSelect =document.getElementById('choix1');
     const statut = ["Nouveau", "En cours", "Terminé"];
     let tasks = [];
-
+    if (!localStorage.getItem ('task')) {
+      localStorage.setItem ('task', JSON.stringify(tasks));
+    }
     submitButton.addEventListener("click", function () {
         const categorieValue = categories[categorieSelect.selectedIndex];
       const titreValue = titreInput.value;
@@ -19,13 +21,13 @@ const statutValue = statut[statutSelect.selectedIndex];
       if (categorieValue && titreValue && dateValue) {
         // Ajouter la tâche au tableau
         tasks.push({ categorie: categorieValue, titre: titreValue, date: dateValue, description: descrValue, statut:statutValue});
-
         // Mettre à jour le tableau HTML
         updateTable();
 
         // Effacer les champs après l'ajout
         titreInput.value = "";
         dateInput.value = "";
+        descr.value = "";
       } else {
         alert("Veuillez remplir tous les champs !");
       }
@@ -37,29 +39,30 @@ const statutValue = statut[statutSelect.selectedIndex];
       <td>#</td>
       <td>Date</td>
       <td>Titre</td>
-      <td class="cat">Catyegorie</td>
+      <td class="cat">Categorie</td>
       <td>Operation</td>
     </tr>`
 
       // Remplir le tableau HTML avec les tâches
       tasks.forEach(function (task, index) {
         const row = tbody.insertRow();
-        // row.innerHTML += `
-        // //   <td class="index" >${index + 1}</td>
-        // //   <td class="date">${task.date}</td>
-        // //   <td class="titre">${task.titre}</td>
-        // //   <td class="categorie">${task.categorie}</td>
-        // `;
+        if (!localStorage.getItem ('row')) {
+          localStorage.setItem ('row', JSON.stringify(row));
+        }
         row.innerHTML +=`
                     <td>${index + 1}</td>
                     <td>${task.date}</td>
                     <td>${task.titre}</td>
                     <td class="cat">${task.categorie}</td>
-                    <td class="cat"><i class="fa fa-trash" aria-hidden="true" onclick="deleteline(${index})"> </i></td>
+                    <td class="cat">
+                    <i class="bi bi-eye"></i>
+                    <i class="bi1 bi-pencil-fill"></i>
+                    <i class="fa fa-trash" aria-hidden="true" onclick="deleteline(${index})"> </i>
+                     </td>
 
               `
+              localStorage.setItem('task', JSON.stringify (task));
       });
-      console.log(tasks)
     }
   });
   
